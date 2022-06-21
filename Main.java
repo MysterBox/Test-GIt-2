86286c7ecb09ab3ca2f82888458a9a27c15a5535
MysterBox <yoganbelajar4@gmail.com>
2022-06-22 01:04:24 +0800

    menambahkan fiture Scanner di Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Scanner AmbilUser = new Scanner (System.in);
+        String Tai = AmbilUser.nextLine();
+        System.out.println(Tai);
+        AmbilUser.close();
     }
 
 }
