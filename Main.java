87b3e71a9b5741bb543b7519ade5fa9f17f0f521
MysterBox <yoganbelajar4@gmail.com>
2022-06-22 01:27:32 +0800

    mengembalikan fiture scanner di Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("kamu bau");
-        System.out.println("aku juga bau");
-        System.out.println("kita semua gak bau");
-
-
-
+        Scanner AmbilUser = new Scanner (System.in);
+        String Tai = AmbilUser.nextLine();
+        System.out.println(Tai);
+        AmbilUser.close();
     }
 
 }
